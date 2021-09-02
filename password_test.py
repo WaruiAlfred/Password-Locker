c2e4d16937a6d48c83e199a14d5f8791e8785597
Alfred Warui <alfredwarui17@gmail.com>
2021-09-03 00:55:44 +0300

    add class and unittest module imports and create a test class

---
+++ password_test.py
@@ -0,0 +1,12 @@
+from user import User #Importing the user class
+from credentials import Credentials #Importing the credentials class
+import unittest #Importing python unittest module for testing
+
+class TestPassword(unittest.TestCase): 
+  '''
+  Test class to test user and credentials classes behaviours.
+  TestCase class helps in creating test cases.
+  '''
+  
+ 
+    
