86d1b93b877e374404ee4c02b4bbc248a6bf13e7
Alfred Warui <alfredwarui17@gmail.com>
2021-09-03 15:35:32 +0300

    add more instance variables and a save function

--- user.py
+++ user.py
@@ -3,8 +3,16 @@ class User:
   class that generates new instances of username
   '''
   
-  username_list = [] #Empty username_list for storing usernames
+  users_list = [] #Empty username_list for storing usernames
   
-  def __init__(self,u_name):
+  def save_user(self): 
+    '''
+    save_user function adds new user(s) official name(S) to users list
+    '''
+    User.users_list.append(self)
+  
+  def __init__(self,o_name,u_name,p_word):
     #Properties for objects instantiating user class defined
+    self.official_name = o_name
     self.username = u_name
+    self.password = p_word
