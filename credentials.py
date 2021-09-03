e63287555b64d154d091aba5d9d92c7bc5684fdc
Alfred Warui <alfredwarui17@gmail.com>
2021-09-03 16:08:52 +0300

    add save function to save credentials

--- credentials.py
+++ credentials.py
@@ -5,6 +5,14 @@ class Credentials:
   
   credentials_list = [] #Credentials list to store user(s) password(s)
   
-  def __init__(self,p_word):
+  def save_credentials(self): 
+    '''
+    this function adds a user's details of account creation to credentials list
+    '''
+    Credentials.credentials_list.append(self)
+  
+  def __init__(self,account,u_name,p_word):
     #Properties for objects instantiating credentials class defined
+    self.account = account
+    self.username = u_name
     self.password = p_word
