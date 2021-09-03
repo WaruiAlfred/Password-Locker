ad38afbd0b5cc93f67f7402fe17ff72c92e568e9
Alfred Warui <alfredwarui17@gmail.com>
2021-09-03 16:09:55 +0300

    add tests for credentials initialization and saving

--- password_test.py
+++ password_test.py
@@ -1,4 +1,5 @@
 from user import User #Importing the user class
+from credentials import Credentials #Importing the credentials class
 import unittest #Importing python unittest module for testing
 
 class TestPassword(unittest.TestCase): 
@@ -12,14 +13,14 @@ class TestPassword(unittest.TestCase):
     setUp method to run before every test case.
     '''
     self.new_user = User("John Mac","Miley","1234")
-    
+    self.new_credentials = Credentials("twitter","Aldis","1234")
     
   def tearDown(self):
     '''
     tearDown method to clean up after each test case has run.
     '''
     User.users_list = []
-   
+    Credentials.credentials_list = []
     
   def test_init(self):
     '''
@@ -29,15 +30,18 @@ class TestPassword(unittest.TestCase):
     self.assertEqual(self.new_user.official_name,"John Mac")
     self.assertEqual(self.new_user.username,"Miley")
     self.assertEqual(self.new_user.password,"1234")
-   
+    self.assertEqual(self.new_credentials.account,"twitter")
+    self.assertEqual(self.new_credentials.username,"Aldis")
+    self.assertEqual(self.new_credentials.password,"1234")
     
   def test_save_user(self): 
     '''
     test_save_user to test if username in added to username list and credentials to credentials list
     '''
     self.new_user.save_user() #save user(s) official name(s)
+    self.new_credentials.save_credentials() #save new credentials for user(s)
     self.assertEqual(len(User.users_list),1) #checking if users list length has increased
-   
+    self.assertEqual(len(Credentials.credentials_list),1) #checking if credentials list length has increased
     
 if __name__ == '__main__':
     unittest.main()   
