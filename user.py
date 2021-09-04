890111fc8f447a8dfa0d3e25df9ff5ac17aae69b
Alfred Warui <alfredwarui17@gmail.com>
2021-09-04 21:58:17 +0300

    create a method to find a user's account by their password

--- user.py
+++ user.py
@@ -11,6 +11,15 @@ class User:
     '''
     User.users_list.append(self)
   
+  @classmethod
+  def find_by_password(cls,password_input): 
+    '''
+    Method that takes in a password and returns true if the password matches the user's account password
+    '''
+    for found in cls.users_list:
+      if found.password == password_input:
+        return found
+  
   def __init__(self,o_name,u_name,p_word):
     #Properties for objects instantiating user class defined
     self.official_name = o_name
