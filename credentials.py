d598dcd88d495b612a13c6f30652bcd7e6235970
Alfred Warui <alfredwarui17@gmail.com>
2021-09-04 21:57:36 +0300

    create a find credential method

--- credentials.py
+++ credentials.py
@@ -10,6 +10,15 @@ class Credentials:
     this function adds a user's details of account creation to credentials list
     '''
     Credentials.credentials_list.append(self)
+    
+  @classmethod
+  def find_credential(cls,account_name_input): 
+    '''
+    Method that takes in account name and displays details of the credential
+    '''
+    for found in cls.credentials_list:
+      if found.account == account_name_input:
+        return found
   
   def __init__(self,account,u_name,p_word):
     #Properties for objects instantiating credentials class defined
